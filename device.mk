9a464cfcdefb16b757fb64a048f805c0f8c17ab8
Sicknemesis <hulluhalonen10@gmail.com>
2015-02-13 22:45:47 +0200

    Change density to 360 dpi

--- device.mk
+++ device.mk
@@ -239,7 +239,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.opengles.version=196608
 
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.sf.lcd_density=480
+    ro.sf.lcd_density=360
 
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.hwc.mdpcomp.enable=true
